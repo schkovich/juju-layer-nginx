58d8c054fec124e562c4372e6df7db59155cd9d2
Goran Miskovic <schkovich@gmail.com>
2018-05-12 17:24:05 +0200

    switch to nginx package instead of nginx-full set port

--- reactive/nginx.py
+++ reactive/nginx.py
@@ -10,24 +10,23 @@ from charmhelpers.core import hookenv
 
 config = hookenv.config()
 
-
 # Handlers --------------------------------------------------------------------
 @when('apt.installed.nginx-full')
+
 @when_not('nginx.available')
 def nginx_ready():
     nginx.remove_default_site()
     hookenv.status_set('active', 'NGINX is ready')
-    set_state('nginx.available')
-
-
-# Example website.available reaction ------------------------------------------
-"""
-This example reaction for an application layer which consumes this nginx layer.
-If left here then this reaction may overwrite your top-level reaction depending
-on service names, ie., both nginx and ghost have the same reaction method,
-however, nginx will execute since it's a higher precedence.
+    set_state('nginx.started')
 
 @when('nginx.available', 'website.available')
 def configure_website(website):
     website.configure(port=config['port'])
+
+# Example website.available reaction ------------------------------------------
+"""
+action for an application layer which consumes this nginx layer.
+If left here then this reaction may overwrite your top-level reaction depending
+on service names, ie., both nginx and ghost have the same reaction method,
+however, nginx will execute since it's a higher precedence.
 """
