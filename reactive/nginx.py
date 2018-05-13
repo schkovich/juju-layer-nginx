7ebd662f7f4d6f58fbd52ffc58ae5a9e89452fe9
Goran Miskovic <schkovich@gmail.com>
2018-05-13 10:26:50 +0200

    removed reaction on nginx and website ready.

--- reactive/nginx.py
+++ reactive/nginx.py
@@ -11,22 +11,22 @@ from charmhelpers.core import hookenv
 config = hookenv.config()
 
 # Handlers --------------------------------------------------------------------
-@when('apt.installed.nginx-full')
-
+@when('apt.installed.nginx')
 @when_not('nginx.available')
 def nginx_ready():
     nginx.remove_default_site()
     hookenv.status_set('active', 'NGINX is ready')
     set_state('nginx.started')
 
+# Example website.available reaction ------------------------------------------
+"""
+This example reaction for an application layer which consumes this nginx layer.
+If left here then this reaction may overwrite your top-level reaction depending
+on service names, ie., both nginx and ghost have the same reaction method,
+however, nginx will execute since it's a higher precedence.
+
 @when('nginx.available', 'website.available')
 def configure_website(website):
     website.configure(port=config['port'])
 
-# Example website.available reaction ------------------------------------------
-"""
-action for an application layer which consumes this nginx layer.
-If left here then this reaction may overwrite your top-level reaction depending
-on service names, ie., both nginx and ghost have the same reaction method,
-however, nginx will execute since it's a higher precedence.
 """
