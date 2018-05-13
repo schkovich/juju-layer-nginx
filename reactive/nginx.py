1743f207932e74ef60ffe05ee3f3228040820db4
Goran Miskovic <schkovich@gmail.com>
2018-05-13 10:26:50 +0200

    removed reaction on nginx and website ready.

--- reactive/nginx.py
+++ reactive/nginx.py
@@ -13,17 +13,18 @@ config = hookenv.config()
 @when('apt.installed.nginx')
 @when_not('nginx.available')
 def nginx_ready():
-    hookenv.status_set('active', 'NGINX is ready')
-    set_state('nginx.started')
+    hookenv.status_set('active', 'NGINX is ready.')
+    set_state('nginx.available')
+
+# Example website.available reaction ------------------------------------------
+"""
+This example reaction for an application layer which consumes this nginx layer.
+If left here then this reaction may overwrite your top-level reaction depending
+on service names, ie., both nginx and ghost have the same reaction method,
+however, nginx will execute since it's a higher precedence.
 
 @when('nginx.available', 'website.available')
 def configure_website(website):
     website.configure(port=config['port'])
 
-# Example website.available reaction ------------------------------------------
-"""
-action for an application layer which consumes this nginx layer.
-If left here then this reaction may overwrite your top-level reaction depending
-on service names, ie., both nginx and ghost have the same reaction method,
-however, nginx will execute since it's a higher precedence.
 """
